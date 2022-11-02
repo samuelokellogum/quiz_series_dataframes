4fd179e40cbd534ed2da097dc49903ecd39a46b7
Samuel Okellogum <oksam21@gmail.com>
2022-11-02 16:37:23 +0300

    add new values

--- frames.py
+++ frames.py
@@ -3,3 +3,9 @@ from urllib.request import urlopen
 from zipfile import ZipFile
 
 url = 'http://'
+
+
+#Question 5
+#Create a dataframe from this link https://jsonplaceholder.typicode.com/albums
+dff = pd.DataFrame(requests.get('https://jsonplaceholder.typicode.com').json())
+print(f'Dataframe is {dff}')
