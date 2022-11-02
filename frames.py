1a6554c817899e61daeb0fdaa29a44e0442940fa
Samuel Okellogum <oksam21@gmail.com>
2022-11-02 16:45:09 +0300

    add new values

--- frames.py
+++ frames.py
@@ -4,6 +4,19 @@ from zipfile import ZipFile
 
 url = 'http://'
 
+#Question 1
+# What is the result of the code
+
+#Question 2
+# Download a CSV file from the link and load it into a dataframe
+url = ''
+df = pd.read_csv(url)
+
+#Question 3
+
+
+#Question 4
+
 
 #Question 5
 #Create a dataframe from this link https://jsonplaceholder.typicode.com/albums
